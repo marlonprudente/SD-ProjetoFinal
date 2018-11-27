b2763f7d0f42239b90d5cac756a273bfb7d24ebd
Marlon <marlonoliveira@alunos.utfpr.edu.br>
2018-11-27 15:05:19 -0200

    Primeiro commit

---
+++ Cliente/src/main/java/com/marlonprudente/interfaces/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * Este sotfware foi feito para a UTFPR - Campus Curitiba;
+ * O Código é livre para uso não comercial;
+ * Desenvolvido através do Netbeans IDE.
+ */
+package com.marlonprudente.interfaces;
+
+/**
+ *
+ * @author Marlon Prudente <marlon.oliveira at alunos.utfpr.edu.br>
+ */
+public interface Cliente {
+    
+}
---
+++ ServidorHoteis/src/main/java/com/marlonprudente/interfaces/ServidorHoteis.java
@@ -0,0 +1,16 @@
+/*
+ * Este sotfware foi feito para a UTFPR - Campus Curitiba;
+ * O Código é livre para uso não comercial;
+ * Desenvolvido através do Netbeans IDE.
+ */
+package com.marlonprudente.interfaces;
+
+import java.rmi.Remote;
+
+/**
+ *
+ * @author Marlon Prudente <marlon.oliveira at alunos.utfpr.edu.br>
+ */
+public interface ServidorHoteis extends Remote{
+    
+}
